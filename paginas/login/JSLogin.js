17c5f5fc9fb637475f91f844150799c81bebfb22
futeboli <leonardoduarte2744@gmail.com>
2025-05-19 09:05:17 -0300

    att

--- paginas/login/JSLogin.js
+++ paginas/login/JSLogin.js
@@ -2,5 +2,5 @@ function signUP(){
     var nome = document.getElementById ("loginNOME").value;
     var email = document.getElementById ("loginEMAIL").value;
     var senha = document.getElementById ("loginSENHA").value;
-    document.getElementById("txtLogin").innerHTML = "Olá " + nome + ", com " + email + " anos da cidade " + senha + ". Seja BEM VINDO á ADA!";
+    document.getElementById("txtLogin").innerHTML = "Olá '" + nome + "', seu email é '" + email + "' e sua senha é '" + senha + "'. Seja BEM VINDO á YGGDRASIL!";
 }
