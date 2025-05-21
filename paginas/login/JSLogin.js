d613d3baa9e7daa8b9f2285c0cf81a67907314bb
futeboli <leonardoduarte2744@gmail.com>
2025-05-21 11:26:00 -0300

    att login

--- paginas/login/JSLogin.js
+++ paginas/login/JSLogin.js
@@ -1,6 +1,5 @@
 function signUP(){
-    var nome = document.getElementById ("loginNOME").value;
     var email = document.getElementById ("loginEMAIL").value;
-    var senha = document.getElementById ("loginSENHA").value;
-    document.getElementById("txtLogin").innerHTML = "Olá '" + nome + "', seu email é '" + email + "' e sua senha é '" + senha + "'. Seja BEM VINDO á YGGDRASIL!";
+    var senha = document.getElementById ("loginPASSWORD").value;
+    document.getElementById("txtLogin").innerHTML = "Olá, seu email é '" + email + "' e sua senha é '" + senha + "'. Seja BEM VINDO á YGGDRASIL!";
 }
