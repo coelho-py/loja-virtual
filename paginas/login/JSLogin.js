679650ab947087de0e65d60a285a555d358c7957
futeboli <leonardoduarte2744@gmail.com>
2025-05-22 11:20:40 -0300

    remove login js

--- paginas/login/JSLogin.js
+++
@@ -1,5 +0,0 @@
-function signUP(){
-    var email = document.getElementById ("loginEMAIL").value;
-    var senha = document.getElementById ("loginPASSWORD").value;
-    document.getElementById("txtLogin").innerHTML = "Olá, seu email é '" + email + "' e sua senha é '" + senha + "'. Seja BEM VINDO á YGGDRASIL!";
-}
