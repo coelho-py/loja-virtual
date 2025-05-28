6de0a93ea9dde18aa132c080312ab184129502a1
Henrique Rugeri <henriquecrugeri@gmail.com>
2025-05-28 10:27:12 -0300

    php pq o janjan quer

--- php/index.php
+++ php/index.php
@@ -1,3 +1,17 @@
 <?php
+// coneccao do banco de dados no php
+$user = "root";
+$password = "";
+$dbName = "loja";
+$host = "localhost";
 
+try {
+  // metodo de conexao do banco de dados em php
+  $pdo = new PDO("mysql:host=$host; dbName=$dbName, $usuario, $senha");
+
+} catch (PDOException $err) {
+
+   echo $err->getMessage();
+
+}
 ?>
