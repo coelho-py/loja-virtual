17864963abb790bf49e802befa0b0012592bf913
matheusmistrineirobraz <math.braz@outlook.com>
2025-05-28 11:20:39 -0300

    linkando a pgina favoritos teste request

--- autenticacao/contas/urls.py
+++ autenticacao/contas/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('registro/', views.registro_view, name='registro'),
-    path('registro/', views.registro_view, name='registro'),
+    path('favoritos/', views.favoritos_view, name='favoritos'),
     path('', views.home_view, name='home'),
 ]
