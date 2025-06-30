8d69c329bdeebd8cd4d009a3b3c604f237399df9
Henrique Rugeri <henriquecrugeri@gmail.com>
2025-06-30 09:03:47 -0300

    integracao ao postgreSQL

--- autenticacao/autenticacao/settings.py
+++ autenticacao/autenticacao/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'autenticacao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'postgres',
-        #'USER': 'postgres',
-        #'PASSWORD': 'janjan69',
-        #'HOST': 'localhost',
-        #'PORT': '5432'
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'janjan69',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
